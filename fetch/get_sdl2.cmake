4e3a5c4197e18008fe778c86e7b6ba92d3fa9e6d
ThePythonator <49040244+ThePythonator@users.noreply.github.com>
2024-09-16 14:08:13 +0100

    Refactor CMakeLists.txt and SDL2 fetch script
    Also makes sure target names are consistent.

--- fetch/get_sdl2.cmake
+++ fetch/get_sdl2.cmake
@@ -1,4 +1,4 @@
-# defines SDL2::SDL2, SDL2::SDL2main and SDL2::image targets
+# Defines SDL2::SDL2, SDL2::SDL2main, SDL2_image::SDL2_image, and SDL2_mixer::SDL2_mixer targets
 
 if(EMSCRIPTEN)
     # Emscripten-specific magic
@@ -10,49 +10,44 @@ if(EMSCRIPTEN)
 
     add_library(SDL2::SDL2main INTERFACE IMPORTED)
 
-    add_library(SDL2::image INTERFACE IMPORTED)
+    add_library(SDL2_image::SDL2_image INTERFACE IMPORTED)
     set_target_properties(SDL2_image::SDL2_image PROPERTIES
         INTERFACE_COMPILE_OPTIONS "SHELL:-s USE_SDL_IMAGE=2"
         INTERFACE_LINK_LIBRARIES "-s USE_SDL_IMAGE=2"
     )
 else()
-    # Check for system SDL2
-    # Currently breaks for me
-    find_package(SDL2 QUIET NO_SYSTEM_ENVIRONMENT_PATH)
-    find_package(SDL2_image QUIET) # will probably fail
-endif()
+    # Build from source
+    include(FetchContent)
 
-# Didn't find it, build from source
-include(FetchContent)
+    if(NOT TARGET SDL2::SDL2)
+        message("Did not find SDL2, building from source.")
 
-if(NOT TARGET SDL2::SDL2)
-    message("Did not find SDL2, building from source.")
+        FetchContent_Populate(SDL2
+            GIT_REPOSITORY https://github.com/libsdl-org/SDL
+            GIT_TAG        release-2.30.5
+        )
+        add_subdirectory(${sdl2_SOURCE_DIR} SDL2 EXCLUDE_FROM_ALL)
+    endif()
 
-    FetchContent_Populate(SDL2
-        GIT_REPOSITORY https://github.com/libsdl-org/SDL
-        GIT_TAG        release-2.30.5
-    )
-    add_subdirectory(${sdl2_SOURCE_DIR} SDL2 EXCLUDE_FROM_ALL)
-endif()
+    if(NOT TARGET SDL2_image::SDL2_image)
+        # get SDL2_image (no released version has CMake support)
+        FetchContent_Populate(SDL2_image
+            GIT_REPOSITORY https://github.com/libsdl-org/SDL_image
+            GIT_TAG        release-2.8.2
+        )
+        add_subdirectory(${sdl2_image_SOURCE_DIR} SDL2_image EXCLUDE_FROM_ALL)
+    endif()
 
-if(NOT TARGET SDL2::image)
-    # get SDL2_image (no released version has CMake support)
-    FetchContent_Populate(SDL2_image
-        GIT_REPOSITORY https://github.com/libsdl-org/SDL_image
-        GIT_TAG        release-2.8.2
-    )
-    add_subdirectory(${sdl2_image_SOURCE_DIR} SDL2_image EXCLUDE_FROM_ALL)
-endif()
+    if(NOT TARGET SDL2_mixer::SDL2_mixer)
+        set(SDL2MIXER_OPUS OFF CACHE BOOL "" FORCE)
+        set(SDL2MIXER_MOD OFF CACHE BOOL "" FORCE)
+        set(SDL2MIXER_MIDI OFF CACHE BOOL "" FORCE)
+        set(SDL2MIXER_WAVPACK OFF CACHE BOOL "" FORCE)
 
-if(NOT TARGET SDL2::mixer)
-    set(SDL2MIXER_OPUS OFF CACHE BOOL "" FORCE)
-    set(SDL2MIXER_MOD OFF CACHE BOOL "" FORCE)
-    set(SDL2MIXER_MIDI OFF CACHE BOOL "" FORCE)
-    set(SDL2MIXER_WAVPACK OFF CACHE BOOL "" FORCE)
-
-    FetchContent_Populate(SDL2_mixer
-        GIT_REPOSITORY https://github.com/libsdl-org/SDL_mixer
-        GIT_TAG        release-2.8.0
-    )
-    add_subdirectory(${sdl2_mixer_SOURCE_DIR} SDL2_mixer EXCLUDE_FROM_ALL)
+        FetchContent_Populate(SDL2_mixer
+            GIT_REPOSITORY https://github.com/libsdl-org/SDL_mixer
+            GIT_TAG        release-2.8.0
+        )
+        add_subdirectory(${sdl2_mixer_SOURCE_DIR} SDL2_mixer EXCLUDE_FROM_ALL)
+    endif()
 endif()
